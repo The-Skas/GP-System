24bcb3e0f8d4ef2660e0bb27d7728bef1b368aac
Milka Horozova <falkeura@gmail.com>
2014-01-13 18:21:48 +0000

    a little NetBeans SVN test

---
+++ SVNtest/src/svntest/SVNtest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package svntest;
+
+/**
+ *
+ * @author Milka
+ */
+public class SVNtest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a NetBeans SVN test");
+    }
+    
+}
